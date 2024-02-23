f6e7c7009e8ff27d78d30183db843a44e2d0a7ca
Tejas <64474868+tejasm05@users.noreply.github.com>
2024-02-23 16:01:47 +0530

    Update main.c

--- NCERT DISCRETE/Assignment 2(Discrete)/codes/main.c
+++ NCERT DISCRETE/Assignment 2(Discrete)/codes/main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-
+#define SIZE 51
 double sum2(int n) {
     return 2.0 * n * (n+1) + 10.0 * (n+1);
 }
 int x(int n) {
     return (4*n+10);
 }
-int u(int n) {
-    return (n >= 0) ? 1 : 0; // Unit step function u(n)
-}
 
+void convolution(int input1[], int input2[], int output[], int size) {
+    for (int i = 0; i < size; i++) {
+        output[i] = 0;
+        for (int j = 0; j <= i; j++) {
+            output[i] += input1[j] * input2[i - j];
+        }
+    }
+}
 
 int main() {
     FILE *fp;
@@ -31,6 +36,35 @@ int main() {
     for (n = 0; n < 15; n++) {
         fprintf(fp, "%d\n", x(n));
     }
+
+    // 2nd part of code DOING CONVOLUTION TO OBTAIN SUM OF N TERMS AND STORING VALUES IN TEXT FILE
+
+    int size = SIZE;
+    int u_n[SIZE], conv_output[SIZE];
+
+    // Initialize u(n)
+    for (int i = 0; i < size; i++) {
+        u_n[i] = (i >= 0) ? 1 : 0;
+    }
+
+    // Generate (10+6n)u(n)
+    int input1[SIZE];
+    for (int i = 0; i < size; i++) {
+        input1[i] = (10 + 4 * i) * u_n[i];
+    }
+
+    // Convolve (10+6n)u(n) with u(n)
+    convolution(input1, u_n, conv_output, size);
+
+    // Headers 
+    fprintf(fp, "n y(n)\n");
+
+
+    // Store the result in a text file
+    for (int i = 0; i < size; i++) {
+        fprintf(fp, "%d %d\n",i, conv_output[i]);
+    }
+    
     fclose(fp);
 
     return 0;
